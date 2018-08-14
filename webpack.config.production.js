5de800e2903dda239d1b30050da30f846ab8e806
Turashov Ivan <turashov@element-soft.com>
2018-08-14 15:25:33 +0300

    Production config

---
+++ webpack.config.production.js
@@ -0,0 +1,69 @@
+/**
+ * Created by turashov on 14.08.2018.
+ */
+
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const port = process.env.PORT || 3000;
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'static/bundle.[hash].js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: true,
+                                importLoaders: 1,
+                                camelCase: true,
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                config: {
+                                    ctx: {
+                                        autoprefixer: {
+                                            browsers: 'last 2 versions'
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    ]
+                })
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'public/index.html',
+            favicon: 'public/favicon-40.png'
+        }),
+        new ExtractTextPlugin({
+            filename: 'styles/styles.[hash].css',
+            allChunks: true
+        })
+    ]
+};
