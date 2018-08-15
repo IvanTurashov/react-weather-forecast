215851c4a17e74d3a99ed4da1fe5ccb8a28cd515
ivan <cat-da-vinci@yamdex.ru>
2018-08-15 19:55:27 +0300

    Demo

--- src/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by ivan on 03.06.18.
- */
-
-import React from 'react';
-import ReactDom from 'react-dom';
-import App from './components/App';
-
-ReactDom.render(<App />, document.getElementById('app'));
